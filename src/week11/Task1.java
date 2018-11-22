4b221623496f0b7325851cbb95e50635912cb3e3
lopinel <ntt17499@gmail.com>
2018-11-22 21:28:21 +0700

    Update week11

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,58 @@
 package week11;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
 public class Task1 {
+    public <T extends Serializable & Comparable> ArrayList<T> task1Sort(ArrayList<T> array){
+        for(int i=0; i<array.size(); i++){
+
+            for(int j=i;j<array.size(); j++){
+                if(array.get(i).compareTo(array.get(j)) > 0){
+                    T te = array.get(i);
+                    array.set(i, array.get(j));
+                    array.set(j, te);
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args){
+        Task1 tk1 = new Task1();
+        ArrayList array = new ArrayList();
+        Byte[] by = {2, 3, 3, 1, 8, 5};
+        Short[] sh = {-1, -3, 5, 6, 2};
+        Character[] ch = {'z', 'c', 'a', 'd', 'm'};
+        Double[] db = {3.14, 5.2, 66.6, -2.3, 10.5};
+
+        ArrayList<Byte> arrayOfByte = new ArrayList<Byte>();
+        for(int i=0; i<by.length; i++){
+            arrayOfByte.add(by[i]);
+        }
+        ArrayList<Short> arrayOfShort = new ArrayList<Short>();
+        for (int i=0; i<sh.length; i++){
+            arrayOfShort.add(sh[i]);
+        }
+        ArrayList<Character> arrayOfChar = new ArrayList<Character>();
+        for (int i=0; i<ch.length; i++){
+            arrayOfChar.add(ch[i]);
+        }
+        ArrayList<Double> arrayOfDouble = new ArrayList<Double>();
+        for(int i=0; i<db.length; i++){
+            arrayOfDouble.add(db[i]);
+        }
+
+        array = tk1.<Byte>task1Sort(arrayOfByte);
+        System.out.println(array.toString());
+
+        array = tk1.<Short>task1Sort(arrayOfShort);
+        System.out.println(array.toString());
+
+        array = tk1.<Character>task1Sort(arrayOfChar);
+        System.out.println(array.toString());
+
+        array = tk1.<Double>task1Sort(arrayOfDouble);
+        System.out.println(array.toString());
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,54 @@
 package week11;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
 public class Task2 {
+    public <T extends Serializable & Comparable> T getMax(ArrayList<T> array){
+        if(array == null || array.size() == 0)
+            return null;
+        T max = array.get(0);
+
+        for(int i=0; i<array.size(); i++){
+            if(max.compareTo(array.get(i)) < 0){
+                max = array.get(i);
+            }
+        }
+        return  max;
+    }
+    
+    public static void main(String[] args){
+        Task2 tk2 = new Task2();
+
+        ArrayList array = new ArrayList();
+        Byte[] by = {2, 3, 3, 1, 8, 5};
+        Short[] sh = {-1, -3, 5, 6, 2};
+        Character[] ch = {'z', 'c', 'a', 'd', 'm'};
+        Double[] db = {3.14, 5.2, 66.6, -2.3, 10.5};
+
+        ArrayList<Byte> arrayOfByte = new ArrayList<Byte>();
+        for(int i=0; i<by.length; i++){
+            arrayOfByte.add(by[i]);
+        }
+        ArrayList<Short> arrayOfShort = new ArrayList<Short>();
+        for (int i=0; i<sh.length; i++){
+            arrayOfShort.add(sh[i]);
+        }
+        ArrayList<Character> arrayOfChar = new ArrayList<Character>();
+        for (int i=0; i<ch.length; i++){
+            arrayOfChar.add(ch[i]);
+        }
+        ArrayList<Double> arrayOfDouble = new ArrayList<Double>();
+        for(int i=0; i<db.length; i++){
+            arrayOfDouble.add(db[i]);
+        }
+
+        System.out.println(tk2.<Byte>getMax(arrayOfByte));
+
+        System.out.println(tk2.<Short>getMax(arrayOfShort));
+
+        System.out.println(tk2.<Character>getMax(arrayOfChar));
+
+        System.out.println(tk2.<Double>getMax(arrayOfDouble));
+    }
 }
